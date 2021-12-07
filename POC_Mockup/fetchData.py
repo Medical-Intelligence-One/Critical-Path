38b13de934c4da88d8ef92a2dd6bf309316c2f11
Tim McLerran <tmclerran@gmail.com>
2021-12-07 14:53:09 -0800

    creating co-occurance probabilities in virtual population

--- POC_Mockup/fetchData.py
+++ POC_Mockup/fetchData.py
@@ -14,7 +14,7 @@ def PotentialComorbidities(cui_prob_list):
     WITH prob2.cui AS CUIs, count(prob2) AS Number
     MATCH (c:Concept)
     WHERE c.cui IN CUIs AND c.cui_pref_term IS NOT NULL 
-    RETURN c.term as `Potential Problem`, c.cui AS CUI, Number
+    RETURN c.term as `PotentialProblem`, c.cui AS CUI, Number
     ORDER BY Number DESC
     '''.format(cui_prob_list=cui_prob_list)
     comorbidities = session.run(query)
@@ -31,7 +31,7 @@ def PotentialComorbidities(cui_prob_list):
     WITH prob2.cui AS CUIs, count(prob2) AS Number
     MATCH (c:Concept)
     WHERE c.cui IN CUIs AND c.cui_pref_term IS NOT NULL 
-    RETURN c.term as `Potential Problem`, c.cui AS CUI, Number
+    RETURN c.term as `PotentialProblem`, c.cui AS CUI, Number
     ORDER BY Number DESC
     '''.format(cui_prob_list=cui_prob_list)
     gen_problems = session.run(query)
@@ -48,14 +48,14 @@ def PotentialComorbidities(cui_prob_list):
     comorbidities = comorbidities[comorbidities['Number'] > 75/len(cui_prob_list)]
     
     # Merge the "Gen_pop_proportion" column from gen_problems into comorbidities
-    comorbidities = pd.merge(comorbidities, gen_problems, on=['CUI', 'Potential Problem'])
+    comorbidities = pd.merge(comorbidities, gen_problems, on=['CUI', 'PotentialProblem'])
     
     comorbidities.head()
     
     comorbidities['OddsRatio'] = (comorbidities['Comorbidities_proportion']/comorbidities['Gen_pop_proportion'])
     comorbidities.sort_values(by='OddsRatio', ascending=False, inplace=True)
     
-    return comorbidities.loc[:,['CUI','Potential Problem', 'OddsRatio']].head(10)
+    return comorbidities.loc[:,['CUI','PotentialProblem', 'OddsRatio']].head(10)
 
 def LikelyAbnormalLabs(cui_prob_list):
     
@@ -64,7 +64,7 @@ def LikelyAbnormalLabs(cui_prob_list):
     WHERE prob1.cui in {cui_prob_list}
     WITH distinct(pt) AS patients
     MATCH (d:D_Labitems)-[:DESCRIBES]->(n:Labevents)<-[:HAD]-(patients)
-    RETURN d.itemid AS ITEMID, d.label as `Abnormal Lab`, d.fluid as `Source`, COUNT(n.flag = 'abnormal') AS abnormal, COUNT(n) as total
+    RETURN d.itemid AS ITEMID, d.label as `AbnormalLab`, d.fluid as `Source`, COUNT(n.flag = 'abnormal') AS abnormal, COUNT(n) as total
     ORDER BY total DESC
     '''.format(cui_prob_list=cui_prob_list)
     with_prob_labs = session.run(query)
@@ -96,7 +96,7 @@ def LikelyAbnormalLabs(cui_prob_list):
     with_prob_labs['OddsRatio'] = (with_prob_labs['with_prob_proportion_abnl']/with_prob_labs['without_prob_proportion_abnl'])
     with_prob_labs.sort_values(by='OddsRatio', ascending=False, inplace=True)
     
-    return with_prob_labs.loc[:,['Abnormal Lab', 'Source', 'OddsRatio']].head(10)
+    return with_prob_labs.loc[:,['AbnormalLab', 'Source', 'OddsRatio']].head(10)
 
 def LikelyPrescriptions(cui_prob_list):
         
