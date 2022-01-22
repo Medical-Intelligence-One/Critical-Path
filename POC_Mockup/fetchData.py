3eb2aaf3bda8763779d820b6db3592c85ccb69e1
Tim McLerran <tmclerran@gmail.com>
2022-01-22 09:47:15 -0800

    updating virtualized population

--- POC_Mockup/fetchData.py
+++ POC_Mockup/fetchData.py
@@ -19,10 +19,10 @@ def PotentialComorbidities(cui_prob_list):
     query = '''
     MATCH p=(ord:Concept)-[r:OCCURS_WITH]->(c:Concept) 
     WHERE c.cui IN {cui_prob_list} AND ord.semantic_type IN ["['Pathologic Function']", "['Disease or Syndrome']", "['Mental or Behavioral Dysfunction']", "['Sign or Symptom']", "['Injury or Poisoning']", "['Neoplastic Process']"]
-    WITH round(r.co_occurrance_probability, 5)*1000 AS Score, ord, r
-    WHERE Score > 20
-    RETURN ord.cui AS `CUI`, ord.term AS `Problem`, Score
-    ORDER BY r.co_occurrance_probability DESC
+    WITH round(r.co_occurrence_probability, 5) AS Score, ord, r, c
+    RETURN c.cui AS `Known_CUI`, c.term AS `Known_Problem`, ord.cui AS `CUI`, ord.term AS `Problem`, Score
+    ORDER BY r.co_occurrence_probability DESC
+    LIMIT 10
     '''.format(cui_prob_list=cui_prob_list)
     data = session.run(query)
     likely_comorbidities = pd.DataFrame([dict(record) for record in data])
@@ -35,10 +35,10 @@ def LikelyOrders(cui_prob_list):
     query = '''
     MATCH p=(ord:Concept)-[r:OCCURS_WITH]->(c:Concept) 
     WHERE c.cui IN {cui_prob_list} AND ord.semantic_type IN ["['Clinical Drug']"]
-    WITH round(r.co_occurrance_probability, 5)*1000 AS Score, ord, r
-    WHERE Score > 20
+    WITH round(r.co_occurrence_probability, 5) AS Score, ord, r
     RETURN ord.cui AS `Code`, ord.term AS `Order`, Score
-    ORDER BY r.co_occurrance_probability DESC
+    ORDER BY r.co_occurrence_probability DESC
+    LIMIT 10
     '''.format(cui_prob_list=cui_prob_list)
     data = session.run(query)
     orders_likely_rx = pd.DataFrame([dict(record) for record in data]).head(10)
@@ -47,10 +47,10 @@ def LikelyOrders(cui_prob_list):
     query = '''
     MATCH p=(ord:Concept)-[r:OCCURS_WITH]->(c:Concept) 
     WHERE c.cui IN {cui_prob_list} AND ord.semantic_type IN ["['Clinical Attribute']"]
-    WITH round(r.co_occurrance_probability, 5)*1000 AS Score, ord, r
-    WHERE Score > 20
+    WITH round(r.co_occurrence_probability, 5) AS Score, ord, r
     RETURN ord.cui AS `Code`, ord.description AS `Order`, Score
-    ORDER BY r.co_occurrance_probability DESC
+    ORDER BY r.co_occurrence_probability DESC
+    LIMIT 10
     '''.format(cui_prob_list=cui_prob_list)
     data = session.run(query)
     orders_likely_lab = pd.DataFrame([dict(record) for record in data]).head(10)
@@ -59,10 +59,10 @@ def LikelyOrders(cui_prob_list):
     query = '''
     MATCH p=(ord:D_Icd_Procedures)-[r:OCCURS_WITH]->(c:Concept) 
     WHERE c.cui IN {cui_prob_list}
-    WITH round(r.co_occurrance_probability, 5)*1000 AS Score, ord, r
-    WHERE Score > 20
+    WITH round(r.co_occurrence_probability, 5) AS Score, ord, r
     RETURN ord.icd9_code AS `Code`, ord.long_title AS `Order`, Score
-    ORDER BY r.co_occurrance_probability DESC
+    ORDER BY r.co_occurrence_probability DESC
+    LIMIT 10
     '''.format(cui_prob_list=cui_prob_list)
     data = session.run(query)
     orders_likely_procedure = pd.DataFrame([dict(record) for record in data]).head(10)
@@ -88,17 +88,26 @@ def nodedisplay():
     test = mi1_neo4jupyter.draw(g,options,query, physics=True)
     return test.data 
 
-def graphdisplay():
+def graphdisplay(kp, p, code, type):
     #NEO4J_CREDS = {'uri': 'neo4j+s://1d23f23f.databases.neo4j.io:7687', 'auth': ('neo4j', 'FUjaBMKHBigyHtjaD9il71GV4GVGAsi7YBWtIBn-Cyo')}
     NEO4J_CREDS = {'uri': 'bolt://76.251.77.235:7687', 'auth': ('neo4j', 'NikeshIsCool')}
     graphistry.register(bolt=GraphDatabase.driver(**NEO4J_CREDS))
-
+    
     g = graphistry.cypher("""
-        MATCH path = (stroke:Concept {cui: 'C0948008'})-[*..4]-(af:Concept {cui: 'C0004238'})
-        RETURN path
-        LIMIT 20
-        """)
+    WITH '{p}' as p, '{code}' as code
+    MATCH path = (stroke:Concept)-[*..4]-(af:Concept) 
+    WHERE stroke.cui = p AND af.cui = code
+    RETURN path
+    LIMIT 20
+    """.format(p=p, code=code))
+    
     g = g.bind(edge_title="type")
-    g = g.bind(point_title="cui")
+    g = g.bind(point_title="term")
+    g = g.settings(url_params={
+    'play': 2000, 'showPointsOfInterest': True, 'showPointsOfInterestLabel': True, 'showLabelPropertiesOnHover': False,
+    'pointsOfInterestMax': 50
+    })
+    g = g.layout_settings(strong_gravity=True, gravity=0.75)
+    g = g.addStyle(logo={'url': 'https://upload.wikimedia.org/wikipedia/commons/4/48/BLANK_ICON.png'})
     iframe_url = g.plot(render=False)
     return iframe_url
