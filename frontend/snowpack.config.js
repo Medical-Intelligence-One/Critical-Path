3f943673661cd88bc90c64d306a1bc24f7411876
Manny Schneck <manny.schneck@gmail.com>
2022-01-21 18:09:55 -0600

    commit generated snowpack config

---
+++ frontend/snowpack.config.js
@@ -0,0 +1,25 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+// SMS: This was generated from `snowpack init`. At this time all of the
+// defaults are fine. We will customize this as our frontend build starts to
+// demand that level of complexity/investment.
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    /* ... */
+  },
+  plugins: [
+    /* ... */
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+};
